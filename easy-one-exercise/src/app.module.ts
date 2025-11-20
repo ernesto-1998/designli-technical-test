75564e775bd026f120776fdbd8d9ebfc207aa0fd
ernesto-1998 <ernestoalonso1998@gmail.com>
2025-11-19 23:25:38 -0600

    feat(common): Implementing global-exception-handler

--- easy-one-exercise/src/app.module.ts
+++ easy-one-exercise/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SesModule } from './modules/ses/ses.module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './common/filter';
 
 @Module({
   imports: [SesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
